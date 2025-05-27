ef9f54635a82d25b7598446a3944f1af36c7a386
Mayank Goel <user@ip-172-31-26-31.ap-south-1.compute.internal>
2025-05-27 21:29:55 +0000

    added other functions to client.cc

--- exercise-2/src_ex1/tcp_echo_client.cc
+++ exercise-2/src_ex1/tcp_echo_client.cc
@@ -6,6 +6,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+const int kPort = 8080;
+const std::string kServerAddress = "127.0.0.1";
+const int kBufferSize = 1024;
+
 std::string getMessageFromArgs(int argc, char* argv[]) {
   std::string message = "Hello from client";
   if (argc > 1) {
@@ -18,54 +22,55 @@ std::string getMessageFromArgs(int argc, char* argv[]) {
   return message;
 }
 
-
-int main(int argc, char* argv[]) {
-  // #Question - are these the same type?
-  // No, string message is a C++ string, while "Hello from client" is a C string (char array)
-
-
-  // std::string message = "Hello from client";
-  // if (argc > 1) {
-  //   message = argv[1];
-  //   for (int i = 2; i < argc; ++i) {
-  //     message += " ";
-  //     message += argv[i];
-  //   }
-  // }
-  std::string message = getMessageFromArgs(argc, argv);
-
-
-
-  const int kPort = 8080;
-  const std::string kServerAddress = "127.0.0.1";
-  sockaddr_in address;
-  const int kBufferSize = 1024;
-  char buffer[kBufferSize] = {0};
-  // Creating socket file descriptor
+int createSocket() {
   int my_sock = socket(AF_INET, SOCK_STREAM, 0);
   if (my_sock < 0) {
-    std::cerr << "Socket creation erron\n";
+    std::cerr << "Socket creation error\n";
     return -1;
   }
+  return my_sock;
+}
+
+bool connectToServer(int my_sock, sockaddr_in& address) {
   address.sin_family = AF_INET;
   address.sin_port = htons(kPort);
-  // Convert IPv4 and IPv6 addresses from text to binary form
   if (inet_pton(AF_INET, kServerAddress.c_str(), &address.sin_addr) <= 0) {
     std::cerr << "Invalid address/ Address not supported\n";
-    return -1;
+    return false;
   }
-  // Connect to the server
-  if (connect(my_sock, (sockaddr *)&address, sizeof(address)) < 0) {
+  if (connect(my_sock, (sockaddr*)&address, sizeof(address)) < 0) {
     std::cerr << "Connection Failed\n";
-    return -1;
+    return false;
   }
-  // Send message
-  send(my_sock, message.c_str(), message.size()+1, 0);
+  return true;
+}
+
+void sendMessage(int my_sock, const std::string& message) {
+  send(my_sock, message.c_str(), message.size() + 1, 0);
   std::cout << "Sent: " << message << "\n";
-  // Wait for reply
+}
+
+void receiveMessage(int my_sock) {
+  char buffer[kBufferSize] = {0};
   ssize_t read_size = read(my_sock, buffer, kBufferSize);
   std::cout << "Received: " << buffer << "\n";
-  // Close the socket
+}
+
+int main(int argc, char* argv[]) {
+  std::string message = getMessageFromArgs(argc, argv);
+
+  int my_sock = createSocket();
+  if (my_sock < 0) return -1;
+
+  sockaddr_in address;
+  if (!connectToServer(my_sock, address)) {
+    close(my_sock);
+    return -1;
+  }
+
+  sendMessage(my_sock, message);
+  receiveMessage(my_sock);
+
   close(my_sock);
   return 0;
 }
