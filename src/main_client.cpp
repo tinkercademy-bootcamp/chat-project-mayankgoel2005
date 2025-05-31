aa40883128534a4bc926bdffa7c74b10fc6203eb
Mayank Goel <user@ip-172-31-26-31.ap-south-1.compute.internal>
2025-05-31 19:40:53 +0000

    added function connect_to_server to main_client.cpp

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -1,4 +1,37 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string>
+#include <cstring>
+
+int connect_to_server(const char* ip, int port) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return -1;
+    }
+
+    sockaddr_in serv_addr;
+    std::memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
+        std::cerr << "Invalid address\n";
+        close(sock);
+        return -1;
+    }
+
+    if (connect(sock, reinterpret_cast<sockaddr*>(&serv_addr), sizeof(serv_addr)) < 0) {
+        perror("connect");
+        close(sock);
+        return -1;
+    }
+
+    return sock;
+}
 
 int main() {
     std::cout << "Hello from client (placeholder)\n";
