7fc507bf5b7bb38a4b2fc32d4f5fcba4d56d18c2
Mayank Goel <user@ip-172-31-26-31.ap-south-1.compute.internal>
2025-05-31 19:31:20 +0000

    added function create_listen_socket to main_server.cpp

--- src/main_server.cpp
+++ src/main_server.cpp
@@ -1,4 +1,45 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string>
+#include <cstring>
+
+int create_listen_socket(int port) {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        perror("socket");
+        return -1;
+    }
+
+    int opt = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt");
+        close(fd);
+        return -1;
+    }
+
+    sockaddr_in addr;
+    std::memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+
+    if (bind(fd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0) {
+        perror("bind");
+        close(fd);
+        return -1;
+    }
+
+    if (listen(fd, 1) < 0) {
+        perror("listen");
+        close(fd);
+        return -1;
+    }
+
+    return fd;
+}
 
 int main() {
     std::cout << "Hello from server (placeholder)\n";
